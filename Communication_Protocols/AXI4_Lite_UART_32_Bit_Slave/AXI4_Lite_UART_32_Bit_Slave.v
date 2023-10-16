f58ca7dbd89071c965c8dee571c6c202b57f2702
Ammar-Bin-Amir <ammar.amiry7@gmail.com>
2023-10-16 18:45:32 +0500

    Modified according to modified AXI4 Lite Slave Adaptor

--- Communication_Protocols/AXI4_Lite_UART_32_Bit_Slave/AXI4_Lite_UART_32_Bit_Slave.v
+++ Communication_Protocols/AXI4_Lite_UART_32_Bit_Slave/AXI4_Lite_UART_32_Bit_Slave.v
@@ -5,70 +5,65 @@ module axi4_lite_uart_32_bit_slave (
     input [31:0] awaddr_in,
     input [2:0] awprot_in,
     input awvalid,
-    output awready_out,
-    input awready_in,
+    output awready,
     // Write Data Channel
     input [31:0] wdata_in,
     input [3:0] wstrb_in,
-    input wvalid_in,
-    output wready_out,
-    input wready_in,
+    input wvalid,
+    output wready,
     // Write Response Channel
-    output [1:0] bresp_out,
-    output bvalid_out,
-    input bready_in,
+    output [1:0] bresp,
+    output bvalid,
+    input bready,
     // Read Address Channel
     input [31:0] araddr_in,
     input [2:0] arprot_in,
-    input arvalid_in,
-    output arready_out,
-    input arready_in,
+    input arvalid,
+    output arready,
     // Read Data Channel
     output [31:0] rdata_out,
-    output [1:0] rresp_out,
-    output rvalid_out,
-    input rready_in,
+    output [1:0] rresp,
+    output rvalid,
+    input rready,
     input [31:0] rdata_in,
     // UART
     output tx,
     input rx
 );
 
-    axi4_lite_slave uut_slave_adapter(
+    axi4_lite_slave_adaptor uut_slave_adaptor(
         .aclk (aclk),
         .aresetn (aresetn),
         // Write Address Channel
         .awaddr_in (awaddr_in),
         .awprot_in (awprot_in),
-        .awvalid_in (awvalid_in),
-        .awready_out (awready_out),
-        .awready_in (awready_in),
+        .awvalid_in (awvalid),
+        .awready_out (awready),
         // Write Data Channel
         .wdata_in (wdata_in),
         .wstrb_in (wstrb_in),
-        .wvalid_in (wvalid_in),
-        .wready_out (wready_out),
-        .wready_in (wready_in),
+        .wvalid_in (wvalid),
+        .wready_out (wready),
         // Write Response Channel
-        .bresp_out (bresp_out),
-        .bvalid_out (bvalid_out),
-        .bready_in (bready_in),
+        .bresp_out (bresp),
+        .bvalid_out (bvalid),
+        .bready_in (bready),
         // Read Address Channel
         .araddr_in (araddr_in),
         .arprot_in (arprot_in),
-        .arvalid_in (arvalid_in),
-        .arready_out (arready_out),
-        .arready_in (arready_in),
+        .arvalid_in (arvalid),
+        .arready_out (arready),
         // Read Data Channel
         .rdata_out (rdata_out),
-        .rresp_out (rresp_out),
-        .rvalid_out (rvalid_out),
-        .rready_in (rready_in),
+        .rresp_out (rresp),
+        .rvalid_out (rvalid),
+        .rready_in (rready),
         .rdata_in (rdata_in)
     );
 
     reg [1:0] address_uart;
     reg we, re;
+    reg rst;
 
     always @(*) begin
         if (awaddr_in[1:0] == 2'b00 || awaddr_in[1:0] == 2'b01 || awaddr_in[1:0] == 2'b10) begin
@@ -79,13 +74,14 @@ module axi4_lite_uart_32_bit_slave (
                 address_uart = araddr_in[1:0];
             end
         end
-        we = (awready_in | wready_in);
-        re = (arready_in);
+        we = (awvalid | wvalid);
+        re = (arvalid);
+        rst = ~aresetn;
     end
 
     uart_32_bit_top_design uut_slave(
         .clk (aclk),
-        .rst (aresetn),
+        .rst (rst),
         .address (address_uart),
         .write_enable (we),
         .write_data (wdata_in),
