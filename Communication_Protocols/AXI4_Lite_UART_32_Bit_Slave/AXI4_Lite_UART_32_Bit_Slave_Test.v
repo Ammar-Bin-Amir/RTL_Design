77e72a8b250868d1818291be8dfe6e1bd22f50f5
Ammar-Bin-Amir <ammar.amiry7@gmail.com>
2023-10-16 18:45:58 +0500

    Modified according to modified AXI4 Lite Slave Adaptor

--- Communication_Protocols/AXI4_Lite_UART_32_Bit_Slave/AXI4_Lite_UART_32_Bit_Slave_Test.v
+++ Communication_Protocols/AXI4_Lite_UART_32_Bit_Slave/AXI4_Lite_UART_32_Bit_Slave_Test.v
@@ -6,116 +6,103 @@ module axi4_lite_uart_32_bit_slave_tb;
     reg aresetn;
     // Write Address Channel
     reg [31:0] awaddr_in;
+    wire [31:0] awaddr_out;
     reg [2:0] awprot_in;
-    reg awvalid_in;
-    wire awready_out;
-    reg awready_in;
+    wire [2:0] awprot_out;
+    wire awvalid;
+    wire awready;
     // Write Data Channel
     reg [31:0] wdata_in;
+    wire [31:0] wdata_out;
     reg [3:0] wstrb_in;
-    reg wvalid_in;
-    wire wready_out;
-    reg wready_in;
+    wire [3:0] wstrb_out;
+    wire wvalid;
+    wire wready;
     // Write Response Channel
-    wire [1:0] bresp_out;
-    wire bvalid_out;
-    reg bready_in;
+    wire [1:0] bresp;
+    wire bvalid;
+    wire bready;
     // Read Address Channel
     reg [31:0] araddr_in;
+    wire [31:0] araddr_out;
     reg [2:0] arprot_in;
-    reg arvalid_in;
-    wire arready_out;
-    reg arready_in;
+    wire [2:0] arprot_out;
     // Read Data Channel
-    wire [31:0] rdata_out;
-    wire [1:0] rresp_out;
-    wire rvalid_out;
-    reg rready_in;
     reg [31:0] rdata_in;
+    wire [31:0] rdata_out;
+    wire [1:0] rresp;
+    wire rvalid;
+    wire rready;
     // UART
     wire tx;
     reg rx;
 
-    wire [31:0] awaddr_out;
-    wire [2:0] awprot_out;
-    wire awvalid_out;
-    wire [31:0] wdata_out;
-    wire [3:0] wstrb_out;
-    wire wvalid_out;
-    wire bready_out;
-    wire [31:0] araddr_out;
-    wire arvalid_out;
-    wire rready_out;
-
-    axi4_lite_master uut_master(
+    axi4_lite_master_adaptor uut_master(
         .aclk (aclk),
         .aresetn (aresetn),
         // Write Address Channel
         .awaddr_out (awaddr_out),
         .awprot_out (awprot_out),
-        .awvalid_out (awvalid_out),
-        .awready_in (awready_out),
+        .awvalid_out (awvalid),
+        .awready_in (awready),
         .awaddr_in (awaddr_in),
         .awprot_in (awprot_in),
         // Write Data Channel
         .wdata_out (wdata_out),
         .wstrb_out (wstrb_out),
-        .wvalid_out (wvalid_out),
-        .wready_in (wready_out),
+        .wvalid_out (wvalid),
+        .wready_in (wready),
         .wdata_in (wdata_in),
         .wstrb_in (wstrb_in),
         // Write Response Channel
-        .bresp_in (bresp_out),
-        .bvalid_in (bvalid_out),
-        .bready_out (bready_out),
+        .bresp_in (bresp),
+        .bvalid_in (bvalid),
+        .bready_out (bready),
         // Read Address Channel
         .araddr_out (araddr_out),
-        .arprot_out (awprot_out),
-        .arvalid_out (arvalid_out),
-        .arready_in (arready_in),
+        .arprot_out (arprot_out),
+        .arvalid_out (arvalid),
+        .arready_in (arready),
         .araddr_in (araddr_in),
-        .arprot_in (awprot_in),
+        .arprot_in (arprot_in),
         // Read Data Channel
         .rdata_in (rdata_out),
-        .rresp_in (rresp_out),
-        .rvalid_in (rvalid_out),
-        .rready_out (rready_out)
+        .rresp_in (rresp),
+        .rvalid_in (rvalid),
+        .rready_out (rready)
     );
     
     axi4_lite_uart_32_bit_slave uut(
-        aclk,
-        aresetn,
+        .aclk (aclk),
+        .aresetn (aresetn),
         // Write Address Channel
-        awaddr_out,
-        awprot_out,
-        awvalid_in,
-        awready_out,
-        awready_in,
+        .awaddr_in (awaddr_out),
+        .awprot_in (awprot_out),
+        .awvalid (awvalid),
+        .awready (awready),
         // Write Data Channel
-        wdata_out,
-        wstrb_out,
-        wvalid_in,
-        wready_out,
-        wready_in,
+        .wdata_in (wdata_out),
+        .wstrb_in (wstrb_out),
+        .wvalid (wvalid),
+        .wready (wready),
         // Write Response Channel
-        bresp_out,
-        bvalid_out,
-        bready_in,
+        .bresp (bresp),
+        .bvalid (bvalid),
+        .bready (bready),
         // Read Address Channel
-        araddr_in,
-        awprot_in,
-        arvalid_in,
-        arready_out,
-        arready_in,
+        .araddr_in (araddr_out),
+        .arprot_in (arprot_out),
+        .arvalid (arvalid),
+        .arready (arready),
         // Read Data Channel
-        rdata_out,
-        rresp_out,
-        rvalid_out,
-        rready_in,
-        rdata_in,
+        .rdata_out (rdata_out),
+        .rresp (rresp),
+        .rvalid (rvalid),
+        .rready (rready),
+        .rdata_in (rdata_in),
         // UART
-        tx,
-        rx
+        .tx (tx),
+        .rx (rx)
     );
 
     initial begin
@@ -132,18 +119,9 @@ module axi4_lite_uart_32_bit_slave_tb;
         aresetn = 1;
         #22 aresetn = 0;
         #128 aresetn = 1;
-        #50 awaddr_in = 0; wdata_in = 130;
-        #50 awvalid_in = 1; wvalid_in = 1;
-        #50 awready_in = 1; wready_in = 1;
-        #50 awvalid_in = 0; awready_in = 0; wvalid_in = 0; wready_in = 0;
-        #50 awaddr_in = 1; wdata_in = 1;
-        #50 awvalid_in = 1; wvalid_in = 1;
-        #50 awready_in = 1; wready_in = 1;
-        #50 awvalid_in = 0; awready_in = 0; wvalid_in = 0; wready_in = 0;
-        #50 awaddr_in = 2; wdata_in = 32'b1111_0000_1011_0100_1010_0101_1001_0110;;
-        #50 awvalid_in = 1; wvalid_in = 1;
-        #50 awready_in = 1; wready_in = 1;
-        #50 awvalid_in = 0; awready_in = 0; wvalid_in = 0; wready_in = 0;
+        #100 awaddr_in = 0; wdata_in = 130;
+        #100 awaddr_in = 1; wdata_in = 1;
+        #100 awaddr_in = 2; wdata_in = 32'b1111_0000_1011_0100_1010_0101_1001_0110;
         // Start
         #104800 rx = 0;
         // Byte 1
@@ -186,70 +164,7 @@ module axi4_lite_uart_32_bit_slave_tb;
         #104800 rx = 1;
         // Idle
         #104800 rx = 1;
-        #50 araddr_in = 3; rdata_in = 32'b1111_0000_1011_0100_1010_0101_1001_0110;;
-        #50 arvalid_in = 1;
-        #50 arready_in = 1; rready_in = 1;
-        #50 arvalid_in = 0; arready_in = 0; rready_in = 0;
-
-        // awaddr_in = 0;
-        // awprot_in = 3;
-        // wdata_in = 130;
-        // wstrb_in = 4'b1111;
-        // araddr_in = 32;
-        // arprot_in = 5;
-        // rdata_in = 32'b1111_0000_1011_0100_1010_0101_1001_0110;
-        // aresetn = 1;
-        // #22 aresetn = 0;
-        // #48 aresetn = 1;
-        // #40 awready_in = 1;
-        // #40 wready_in = 1;
-        // #40 arready_in = 1;
-        // #50 awready_in = 0; wready_in = 0; arready_in = 0;
-        // #30 awready_in = 1;
-        // #10 wready_in = 1;
-        // #10 arready_in = 1;
-        // // Start
-        // #104800 rx = 0;
-        // // Byte 1
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // // Byte 2
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // // Byte 3
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 0;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // // Byte 4
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 1;
-        // #104800 rx = 0;
-        // #104800 rx = 0;
-        // #104800 rx = 0;
-        // #104800 rx = 0;
-        // // Stop
-        // #104800 rx = 1;
-        // // Idle
-        // #104800 rx = 1;
+        #50 araddr_in = 3; rdata_in = 32'b1111_0000_1011_0100_1010_0101_1001_0110;
         #100000000 $finish;
     end
 
