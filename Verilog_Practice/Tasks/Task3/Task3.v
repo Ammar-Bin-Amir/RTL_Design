ae17fbdd4c4252eeef506c33324ad928e80de674
Ammar-Bin-Amir <ammar.amiry7@gmail.com>
2023-10-18 12:10:05 +0500

    States changed and modifies according to the traffic light operation

--- Verilog_Practice/Tasks/Task3/Task3.v
+++ Verilog_Practice/Tasks/Task3/Task3.v
@@ -2,9 +2,10 @@
 
 Design a One-Way Traffic Control System:
 > On RESET every light would be LOW
-> RED lights remains HIGH for 30s after that YELLOW light would be HIGH
-> YELLOW lights remain HIGH for 3s after that GREEN light would be HIGH
-> GREEN lights remain HIGH for 30s after that RED light would be HIGH
+> RED light will remain HIGH for 30s after that YELLOW light would be HIGH
+> YELLOW light will remain HIGH for 3s along with RED light after that GREEN light would be HIGH
+> GREEN light will remain HIGH for 30s after that YELLOW light would be HIGH
+> YELLOW light will remain HIGH for 3s after that RED light would be HIGH
 
 */
 
@@ -16,118 +17,148 @@ module task3 (
     output reg green
 );
     
-    localparam IDLE = 2'b00;
-    localparam RED = 2'b01;
-    localparam YELLOW = 2'b10;
-    localparam GREEN = 2'b11;
+    localparam DISABLE = 3'b000;
+    localparam STOP = 3'b001;
+    localparam READY_TO_GO = 3'b010;
+    localparam GO = 3'b011;
+    localparam READY_TO_STOP = 3'b100;
 
-    reg [1:0] current_state, next_state;
+    reg [2:0] current_state, next_state;
     
     always @(posedge clk) begin
         if (rst) begin
-            current_state <= IDLE;
+            current_state <= DISABLE;
         end
         else begin
             current_state <= next_state;
         end
     end
 
-    reg [4:0] count_red;
+    reg [4:0] count_stop;
 
     always @(posedge clk) begin
         if (rst) begin
-            count_red <= 0;
+            count_stop <= 0;
         end
         else begin
-            if (next_state == RED) begin
-                if (count_red == 30) begin
-                    count_red <= 0;
+            if (next_state == STOP) begin
+                if (count_stop == 30) begin
+                    count_stop <= 0;
                 end
                 else begin
-                    count_red <= count_red + 1;
+                    count_stop <= count_stop + 1;
                 end
             end
             else begin
-                count_red <= 0;
+                count_stop <= 0;
             end
         end
     end
 
-    reg [1:0] count_yellow;
+    reg [1:0] count_ready_to_go;
 
     always @(posedge clk) begin
         if (rst) begin
-            count_yellow <= 0;
+            count_ready_to_go <= 0;
         end
         else begin
-            if (next_state == YELLOW) begin
-                if (count_yellow == 3) begin
-                    count_yellow <= 0;
+            if (next_state == READY_TO_GO) begin
+                if (count_ready_to_go == 3) begin
+                    count_ready_to_go <= 0;
                 end
                 else begin
-                    count_yellow <= count_yellow + 1;
+                    count_ready_to_go <= count_ready_to_go + 1;
                 end
             end
             else begin
-                count_yellow <= 0;
+                count_ready_to_go <= 0;
             end
         end
     end
 
-    reg [4:0] count_green;
+    reg [4:0] count_go;
 
     always @(posedge clk) begin
         if (rst) begin
-            count_green <= 0;
+            count_go <= 0;
         end
         else begin
-            if (next_state == GREEN) begin
-                if (count_green == 30) begin
-                    count_green <= 0;
+            if (next_state == GO) begin
+                if (count_go == 30) begin
+                    count_go <= 0;
                 end
                 else begin
-                    count_green <= count_green + 1;
+                    count_go <= count_go + 1;
                 end
             end
             else begin
-                count_green <= 0;
+                count_go <= 0;
+            end
+        end
+    end
+
+    reg [1:0] count_ready_to_stop;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            count_ready_to_stop <= 0;
+        end
+        else begin
+            if (next_state == READY_TO_STOP) begin
+                if (count_ready_to_stop == 3) begin
+                    count_ready_to_stop <= 0;
+                end
+                else begin
+                    count_ready_to_stop <= count_ready_to_stop + 1;
+                end
+            end
+            else begin
+                count_ready_to_stop <= 0;
             end
         end
     end
 
     always @(*) begin
         case (current_state)
-            IDLE: begin
+            DISABLE: begin
                 red = 0;
                 yellow = 0;
                 green = 0;
-                next_state <= RED;
+                next_state <= STOP;
             end
-            RED: begin
+            STOP: begin
                 red = 1;
                 yellow = 0;
                 green = 0;
-                if (count_red == 30) begin
-                    next_state = YELLOW;
+                if (count_stop == 30) begin
+                    next_state = READY_TO_GO;
                 end
             end
-            YELLOW: begin
+            READY_TO_GO: begin
                 red = 1;
                 yellow = 1;
                 green = 0;
-                if (count_yellow == 3) begin
-                    next_state = GREEN;
+                if (count_ready_to_go == 3) begin
+                    next_state = GO;
                 end
             end
-            GREEN: begin
+            GO: begin
                 red = 0;
                 yellow = 0;
                 green = 1;
-                if (count_green == 30) begin
-                    next_state = RED;
+                if (count_go == 30) begin
+                    next_state = READY_TO_STOP;
                 end
             end
-            default: next_state <= IDLE;
+            READY_TO_STOP: begin
+                red = 0;
+                yellow = 1;
+                green = 0;
+                if (count_ready_to_stop == 3) begin
+                    next_state = STOP;
+                end
+            end
+            default: next_state <= DISABLE;
         endcase
     end
 
