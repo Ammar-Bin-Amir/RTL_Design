5a417e31fb8689978793998cf283f1dc8e2a18dd
Ammar-Bin-Amir <ammar.amiry7@gmail.com>
2023-11-16 05:55:56 +0500

    Inserted Register Interface

--- Communication_Protocols/SPI/SPI_tb.v
+++ Communication_Protocols/SPI/SPI_tb.v
@@ -5,20 +5,20 @@ module spi_tb;
     
     reg clk;
     reg rst;
-    reg en;
-    reg [7:0] ext_command_in;
-    reg [7:0] ext_address_in;
-    reg [7:0] ext_data_in;
-    output wire [7:0] ext_data_out;
+    reg [2:0] address;
+    reg we;
+    reg [31:0] write_data;
+    reg re;
+    wire [31:0] read_data;
 
     spi uut (
         .clk (clk),
         .rst (rst),
-        .en (en),
-        .ext_command_in (ext_command_in),
-        .ext_address_in (ext_address_in),
-        .ext_data_in (ext_data_in),
-        .ext_data_out (ext_data_out)
+        .addr (address),
+        .we (we),
+        .write_data (write_data),
+        .re (re),
+        .read_data (read_data)
     );
 
     initial clk = 0;
@@ -27,25 +27,42 @@ module spi_tb;
     initial begin
         #10 rst = 1;
         #50 rst = 0;
-        #50 ext_command_in = 8'ha5; ext_address_in = 8'h12; ext_data_in = 8'hed;
-        #25 en = 1;
-        #30 en = 0;
-        #1500 ext_command_in = 8'h00; ext_address_in = 8'h34; ext_data_in = 8'hcb;
-        #25 en = 1;
-        #25 en = 0;
-        #1500 ext_command_in = 8'hff; ext_address_in = 8'h67; ext_data_in = 8'h98;
-        #30 en = 1;
-        #30 en = 0;
-        #1500 ext_command_in = 8'h00; ext_address_in = 8'h12; ext_data_in = 8'hed;
-        #25 en = 1;
-        #30 en = 0;
-        #1500 ext_command_in = 8'hff; ext_address_in = 8'h34; ext_data_in = 8'hcb;
-        #25 en = 1;
-        #25 en = 0;
-        #1500 ext_command_in = 8'ha5; ext_address_in = 8'h67; ext_data_in = 8'h98;
-        #30 en = 1;
-        #30 en = 0;
-        #1500 $finish;
+        #50 address = 0; we = 0; write_data = 1;
+        #20 address = 1; we = 1; write_data = 8'ha5;
+        #20 address = 2; we = 1; write_data = 24'h12_34_56;
+        #20 address = 3; we = 1; write_data = 32'h78_9a_bc_de;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 address = 1; we = 1; write_data = 8'h00;
+        #20 address = 2; we = 1; write_data = 24'hf0_12_34;
+        #20 address = 3; we = 1; write_data = 32'h56_78_9a_bc;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 address = 1; we = 1; write_data = 8'hff;
+        #20 address = 2; we = 1; write_data = 24'hde_f0_12;
+        #20 address = 3; we = 1; write_data = 32'h34_56_78_9a;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 address = 4; re = 0;
+        #3000 address = 4; re = 1;
+        #20 address = 1; we = 1; write_data = 8'h00;
+        #20 address = 2; we = 1; write_data = 24'h12_34_56;
+        #20 address = 3; we = 1; write_data = 32'h78_9a_bc_de;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 address = 1; we = 1; write_data = 8'hff;
+        #20 address = 2; we = 1; write_data = 24'hf0_12_34;
+        #20 address = 3; we = 1; write_data = 32'h56_78_9a_bc;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 address = 4; re = 0;
+        #3000 address = 4; re = 1;
+        #3000 address = 1; we = 1; write_data = 8'h5a;
+        #20 address = 2; we = 1; write_data = 24'hde_f0_12;
+        #20 address = 3; we = 1; write_data = 32'h34_56_78_9a;
+        #30 address = 0; we = 1; write_data = 1;
+        #30 address = 0; we = 1; write_data = 0;
+        #3000 $finish;
     end
 
     initial begin
